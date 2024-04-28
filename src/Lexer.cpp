43b800895981651eadd5d008fbaf51db4c862d38
Mobina Shahbazi <shahbazi.mbn@gmail.com>
2024-04-28 21:16:27 +0330

    next function completed

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -24,6 +24,163 @@ namespace charinfo {
 	}
 }
 
+void Lexer::next(Token& token) {
+
+	while (*BufferPtr && charinfo::isWhitespace(*BufferPtr)) {      // Skips whitespace like " "
+		++BufferPtr;
+	}
+
+	if (!*BufferPtr) {              // since end of context is 0 -> !0 = true -> end of context
+		token.Kind = Token::eof;
+		return;
+	}
+
+	if (charinfo::isLetter(*BufferPtr)) {   // looking for keywords or identifiers like "int", a123 , ...
+
+		const char* end = BufferPtr + 1;
+
+		while (charinfo::isLetter(*end) || charinfo::isDigit(*end))
+			++end;                          // until reaches the end of lexeme
+		// example: ".int " -> "i.nt " -> "in.t " -> "int. "
+
+
+		llvm::StringRef Context(BufferPtr, end - BufferPtr);  // start of lexeme, length of lexeme
+
+		Token::TokenKind kind;
+
+		if (Context == "int") {
+			kind = Token::KW_int;
+		}
+        if (Context == "bool") {                           //ch
+			kind = Token::KW_bool;
+		}
+		else if (Context == "if") {
+			kind = Token::KW_if;
+		}
+		else if (Context == "else if") {                    //ch
+			kind = Token::KW_elseif;
+		}
+		else if (Context == "else") {                      //ch
+			kind = Token::KW_else;
+		}
+		else if (Context == "for") {                       //ch
+			kind = Token::KW_for;
+		}
+        else if (Context == "while") {                     //ch
+			kind = Token::KW_while;
+		}
+        else if (Context == "print") {                     //ch
+			kind = Token::KW_print;
+		}
+		else if (Context == "and") {
+			kind = Token::KW_and;
+		}
+		else if (Context == "or") {
+			kind = Token::KW_or;
+		}
+		else if (Context == "true") {
+			kind = Token::KW_true;
+		}
+		else if (Context == "false") {
+			kind = Token::KW_false;
+		}
+		else {
+			kind = Token::ident;
+		}
+
+
+		formToken(token, end, kind);
+		return;
+	}
+
+	else if (charinfo::isDigit(*BufferPtr)) {
+
+		const char* end = BufferPtr + 1;
+
+		while (charinfo::isDigit(*end))
+			++end;
+
+		formToken(token, end, Token::number);
+		return;
+	}
+
+	else {
+
+		if (*BufferPtr == '=' && *(BufferPtr + 1) == '=') {      // ==
+			formToken(token, BufferPtr + 2, Token::equal_equal);
+		}
+
+		else if (*BufferPtr == '+' && *(BufferPtr + 1) == '=') {      // +=
+			formToken(token, BufferPtr + 2, Token::plus_equal);
+		}
+
+		else if (*BufferPtr == '-' && *(BufferPtr + 1) == '=') {      // -=
+			formToken(token, BufferPtr + 2, Token::minus_equal);
+		}
+
+		else if (*BufferPtr == '*' && *(BufferPtr + 1) == '=') {      // *=
+			formToken(token, BufferPtr + 2, Token::star_equal);
+		}
+
+		else if (*BufferPtr == '/' && *(BufferPtr + 1) == '=') {      // /=
+			formToken(token, BufferPtr + 2, Token::slash_equal);
+		}
+
+		else if (*BufferPtr == '%' && *(BufferPtr + 1) == '=') {      // %=
+			formToken(token, BufferPtr + 2, Token::mod_equal);
+		}
+
+		else if (*BufferPtr == '!' && *(BufferPtr + 1) == '=') {      // !=
+			formToken(token, BufferPtr + 2, Token::not_equal);
+		}
+
+		else if (*BufferPtr == '<' && *(BufferPtr + 1) == '=') {      // <=
+			formToken(token, BufferPtr + 2, Token::less_equal);
+		}
+
+		else if (*BufferPtr == '>' && *(BufferPtr + 1) == '=') {      // >=
+			formToken(token, BufferPtr + 2, Token::greater_equal);
+		}
+        else if (*BufferPtr == '/' && *(BufferPtr + 1) == '*') {      // /*
+			formToken(token, BufferPtr + 2, Token::start_comment);
+		}
+        else if (*BufferPtr == '*' && *(BufferPtr + 1) == '/') {      // */
+			formToken(token, BufferPtr + 2, Token::end_comment);
+		}
+		else if (*BufferPtr == '=') {
+			formToken(token, BufferPtr + 1, Token::equal);
+		}
+		else {
+			switch (*BufferPtr) {
+
+#define CASE(ch, tok) \
+            case ch: formToken(token, BufferPtr + 1, tok); break
+
+				CASE('+', Token::plus);
+				CASE('-', Token::minus);
+				CASE('*', Token::star);
+				CASE('/', Token::slash);
+				CASE('(', Token::l_paren);
+				CASE(')', Token::r_paren);
+				CASE(':', Token::colon);
+				CASE(',', Token::comma);
+				CASE('^', Token::power);
+				CASE('>', Token::greater);
+				CASE('<', Token::less);
+				CASE(';', Token::semi_colon);
+				CASE('%', Token::mod);
+                CASE('{', Token::KW_begin);         // ch
+                CASE('}', Token::KW_end);           // ch
+
+#undef CASE
+
+			default:
+				formToken(token, BufferPtr + 1, Token::unknown);
+			}
+			return;
+		}
+	}
+}
 
 void Lexer::setBufferPtr(const char* buffer){
 	BufferPtr = buffer;
