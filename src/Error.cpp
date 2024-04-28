0152b3c1f90cc6af5d8f771abf8197dd157e0eb7
MahdisSep <Mahdissepahvand@gmail.com>
2024-04-28 16:31:57 +0330

    errors of define and assignment has been added.

--- src/Error.cpp
+++ src/Error.cpp
@@ -5,5 +5,15 @@ void Error::SemiColonNotFound()
 	cout << "Semicolon not found...\n";
 	exit(3);
 }
+void Error::DefineInsideScope()
+{
+	cout << "Can't define variable inside scope...\n";
+	exit(3);
+}
 
+void Error::AssignmentEqualNotFound()
+{
+	cout << "Assignment does not have a '=' character...\n";
+	exit(3);
+}
 
