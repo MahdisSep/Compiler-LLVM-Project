915d5dd4cb035a832addaf3cd997838d8b9f4f4e
MahdisSep <Mahdissepahvand@gmail.com>
2024-04-28 16:45:07 +0330

    void function to show different errors.

--- src/Sema.cpp
+++ src/Sema.cpp
@@ -10,7 +10,24 @@ namespace {
         bool HasError;
 
         enum ErrorType { Twice, Not, DivByZero ,MathOp,DiffType };
-
+        
+          void error(ErrorType ET, llvm::StringRef V) {
+            if (ET == ErrorType::DivByZero) {
+                llvm::errs() << "Division by zero is not allowed." << "\n";
+            }
+            else if (ET == ErrorType::MathOp){
+                llvm::errs() << "Boolean is not allowed in math operations." << "\n";
+            }
+            else if (ET == ErrorType::DiffType){
+                llvm::errs() << "Assign different types of variables is not allowed." << "\n";
+            }
+            else
+            {
+                llvm::errs() << "Variable " << V << " is " << (ET == Twice ? "already" : "not") << " declared!\n";
+            }
+            HasError = true;
+            exit(3);
+        }
         
         //not --> expression , assignment , unery
         //twice --> define
