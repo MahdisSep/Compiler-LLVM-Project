fe557c89895d141974eb6799da3a9f1fe5d20af3
MahdisSep <Mahdissepahvand@gmail.com>
2024-05-03 14:49:15 +0330

    add new type for boolean and comment.

--- src/CodeGen.cpp
+++ src/CodeGen.cpp
@@ -15,7 +15,7 @@ namespace
         Module* M;
         IRBuilder<> Builder;
         Type* VoidTy;
-        Type* Int1Ty;
+        Type* Int1Ty; //boolean
         Type* Int32Ty;
         Type* Int8PtrTy;
         Type* Int8PtrPtrTy;
